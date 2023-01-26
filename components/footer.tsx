12f3fb74d9f276289226634eaef14cc61aa2ead4
김태훈 <kth9193@gmail.com>
2023-01-26 11:02:01 +0900

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="flex items-center w-full h-[242px] bg-[#171717] text-white px-[160px]">
-      <ol className="flex flex-col">
+    <footer className="flex justify-center items-center w-full h-[242px] bg-[#171717] text-white ">
+      <ol className="flex flex-col w-[1600px]">
         <li>phone: 010.3968.4209</li>
         <li>email:ama1175@naver.com</li>
       </ol>
